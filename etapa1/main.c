011525705f0cfe6ad8216cd8fd7f3b5dfc2bb134
Fernando Bombardelli da Silva <f.bombardellidasilva@campus.tu-berlin.de>
2016-03-10 01:19:36 +0100

    Estrutura básica

---
+++ etapa1/main.c
@@ -0,0 +1,10 @@
+/* INF01147 -­‐ Compiladores -­‐ 2016/1
+ * Trabalho Prático - Etapa 1
+ * Autores: Fernando Bombardelli da Silva (218324)
+ *          Pedro Faustini ()
+ */
+
+int main(int argc, int *argv[]) {
+  yylex();
+  return 0;
+}
