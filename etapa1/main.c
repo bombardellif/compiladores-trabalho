3d1246455ae11313e2d6f873c77c61fc61852efe
Fernando Bombardelli da Silva <f.bombardellidasilva@campus.tu-berlin.de>
2016-03-10 19:49:51 +0100

    alterações do fork do pedro

--- etapa1/main.c
+++ etapa1/main.c
@@ -1,10 +1,20 @@
 /* INF01147 -­‐ Compiladores -­‐ 2016/1
  * Trabalho Prático - Etapa 1
  * Autores: Fernando Bombardelli da Silva (218324)
- *          Pedro Faustini ()
+ *          Pedro Henrique Arruda Faustini (217432)
+ *  Função com finalidade exclusiva para testes.
  */
 
-int main(int argc, int *argv[]) {
-  yylex();
-  return 0;
+int main(int argc, int *argv[])
+{
+	int ret;
+	initMe();
+	do{
+		ret = yylex();
+		if(ret > 127 && ret > -1) printf("Token lido: %i\n",ret);
+		else printf("Token lido: %c\n",ret);
+	}while(isRunning());
+
+	printf("Numero de linhas: %i \n",getLineNumber());
+    return 0;
 }
